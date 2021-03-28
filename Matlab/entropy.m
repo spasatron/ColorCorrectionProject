f57bd281d81774a8adbdfbca1fa10aaa02c4ac1b
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2021-03-28 14:30:54 -0600

    Update entropy.m

--- Matlab/entropy.m
+++ Matlab/entropy.m
@@ -1,8 +1,9 @@
+% Function to compute the total entropy of a color corrected image
 function [H] = entropy( Gr, Gg, Gb )
     P = imread("lily_small.png");
     Q = double(P) / 255;
+    % Get intensity channel of adjusted image
     Y = 255 * ( 0.299*Q(:,:, 1).^Gr + 0.587*Q(:,:, 2).^Gg + 0.114*Q(:,:,3).^Gb );
     p = nonzeros( histcounts(Y, 256) ) / numel(Y);
-    p = nonzeros( p );
     H = -sum( p.*log2(p) );
 end
