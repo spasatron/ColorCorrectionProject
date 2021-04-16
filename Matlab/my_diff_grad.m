8f8631ef1d619ff22f6814971f03e0144932b687
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2021-04-15 21:20:38 -0600

    Update my_diff_grad.m

--- Matlab/my_diff_grad.m
+++ Matlab/my_diff_grad.m
@@ -1,17 +1,18 @@
 function grad = my_diff_grad( image, g )
     % Scale image, get averages
     S = reshape( image .^ cat( 3, g(1), g(2), g(3) ), [], 3);
-    diff_vec = mean( S );
+    N = size( S, 1 );
+    cv = mean(S) - mean(S, 'all');
 
-    % Get denominator of smooth max
-    grad = 2*ones(3, 1) / ( sum( exp( diff_vec.^2 ) ) - 2);
+    % Create cell array of nonzero values
+    S = { nonzeros(S(:,1)), nonzeros(S(:,2)), nonzeros(S(:,3)) };
     
-    Jac = [0,0,0];
-    for k=1:3
-        Jac(1) = sum( nonzeros(S(:,1)).^g(k) .* log(nonzeros(S(:,1))) );
-        Jac(2) = sum( nonzeros(S(:,2)).^g(k) .* log(nonzeros(S(:,2))) );
-        Jac(3) = sum( nonzeros(S(:,3)).^g(k) .* log(nonzeros(S(:,3))) );
-        
-        grad(k) = grad(k) * dot( Jac .* circshift([-1, -1, 2], k) / size( S, 1 ), exp( diff_vec.^2 ).*diff_vec );
-    end
+    % Get denominator of smooth max
+    grad = 2 * ones(3, 1) / ( sum( exp( cv.^2 ) ) - 2);
+    
+    % Compute gradient as dot product 
+    grad(1) = grad(1)*dot( cellfun( @(Si) sum( Si.^g(1) .* log( Si ) ), S ).*[2, -1, -1]/3/N, exp( cv.^2 ).*cv );
+    grad(2) = grad(2)*dot( cellfun( @(Si) sum( Si.^g(2) .* log( Si ) ), S ).*[-1, 2, -1]/3/N, exp( cv.^2 ).*cv );
+    grad(3) = grad(3)*dot( cellfun( @(Si) sum( Si.^g(3) .* log( Si ) ), S ).*[-1, -1, 2]/3/N, exp( cv.^2 ).*cv );
+    
 end
