ef61eaefb27b0e8b23bd8b7472c3dbb29eaa902b
Spas Angelov <spasangelov@gmail.com>
2021-04-18 17:46:55 -0600

    Soft Max and other functions

---
+++ Matlab/rgbHist.m
@@ -0,0 +1,29 @@
+function rgbhist(I)
+%RGBHIST Histogram of RGB values.
+
+if (size(I, 3) ~= 3)
+error('rgbhist:numberOfSamples', 'Input image must be RGB.')
+end
+
+nBins = 256;
+
+rHist = imhist(I(:,:,1), nBins);
+gHist = imhist(I(:,:,2), nBins);
+bHist = imhist(I(:,:,3), nBins);
+
+%hFig = figure;
+
+figure
+subplot(1,2,1);imshow(I)
+subplot(1,2,2);
+
+h(1) = stem(1:256, rHist); hold on
+h(2) = stem(1:256 + 1/3, gHist);
+h(3) = stem(1:256 + 2/3, bHist);
+hold off
+
+set(h, 'marker', 'none')
+set(h(1), 'color', [1 0 0])
+set(h(2), 'color', [0 1 0])
+set(h(3), 'color', [0 0 1])
+axis square
--- Matlab/spas_convex_test.m
+++ Matlab/spas_convex_test.m
@@ -4,11 +4,32 @@ image = im2double(image);
 
 
 
-gamma0 = ones(2, 1);
-keyboard;
-gammas = fminsearch(@(gamma)colorDifferenceMean(nonlinearScaling(image, gamma(1, 1), gammas(2, 1), gammas(3, 1))), gamma0);
+gamma0 = ones(3, 1);
+gammas = fminsearch(@(gamma)colorDifferenceSoftMax(nonlinearScaling(image, gamma(1, 1), gamma(2, 1), gamma(3, 1)), .000001), gamma0);
+
+
 
 
 f1 = imshow(nonlinearScaling(image, gammas(1, 1), gammas(2, 1), gammas(3, 1)));
 figure;
 f2 = imshow(image);
+
+% A = 0;
+% B = 4;
+% N = 50;
+% 
+% h = (B-A)/(N-1);
+% 
+% x = A:h:B;
+% y = A:h:B;
+% 
+% keyboard;
+% [X, Y] = meshgrid(x, y);
+% Z = zeros(N, N);
+% for i = 1:N
+%     for j = 1:N
+%         Z(i, j) = colorDifferenceMean(nonlinearScaling(image, x(i), 1, y(j)));
+%     end
+% end
+% 
+% mesh(X, Y, Z);
