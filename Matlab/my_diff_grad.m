fc30521bda460c799b788e90cdcbfdd6e9b7f548
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2021-04-21 11:57:00 -0600

    fixed numerical error
    made faster, but more memory intensive

--- Matlab/my_diff_grad.m
+++ Matlab/my_diff_grad.m
@@ -1,18 +1,24 @@
 function grad = my_diff_grad( image, g )
     % Scale image, get averages
-    S = reshape( image .^ cat( 3, g(1), g(2), g(3) ), [], 3);
-    N = size( S, 1 );
-    cv = mean(S) - mean(S, 'all');
-
-    % Create cell array of nonzero values
-    S = { nonzeros(S(:,1)), nonzeros(S(:,2)), nonzeros(S(:,3)) };
+    S = reshape( image, [], 3) .^ (g');
+    cv = mean( S ) - mean( S, 'all' );
+    
+    % Get nonzero pixel values for gradient
+    pixels = reshape( image, [], 3 );
+    N = size( pixels, 1 );
+    
+    % Get nonzero values for each channel
+    Rnz = nonzeros(pixels(:,1));
+    Gnz = nonzeros(pixels(:,2));
+    Bnz = nonzeros(pixels(:,3));
     
     % Get denominator of smooth max
-    grad = 2 * ones(3, 1) / ( sum( exp( cv.^2 ) ) - 2);
+    grad = [sum( Rnz.^g(1) .* log( Rnz ) ) ; 
+            sum( Gnz.^g(1) .* log( Gnz ) ) ; 
+            sum( Bnz.^g(1) .* log( Bnz ) ) ]; 
     
-    % Compute gradient as dot product 
-    grad(1) = grad(1)*dot( cellfun( @(Si) sum( Si.^g(1) .* log( Si ) ), S ).*[2, -1, -1]/3/N, exp( cv.^2 ).*cv );
-    grad(2) = grad(2)*dot( cellfun( @(Si) sum( Si.^g(2) .* log( Si ) ), S ).*[-1, 2, -1]/3/N, exp( cv.^2 ).*cv );
-    grad(3) = grad(3)*dot( cellfun( @(Si) sum( Si.^g(3) .* log( Si ) ), S ).*[-1, -1, 2]/3/N, exp( cv.^2 ).*cv );
-    
+    % Compute gradient
+    grad = 2 * grad / ( sum( exp( cv.^2 ) ) - 2) / 3 / N;
+    grad = grad .* [2, -1, -1; -1, 2, -1; -1, -1, 2 ] * (exp( cv.^2 ).*cv)';
+
 end
