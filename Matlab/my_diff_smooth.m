e2cd301658d8e77489090ab02379cb96f853ecec
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2021-04-21 11:57:50 -0600

    Update my_diff_smooth.m

--- Matlab/my_diff_smooth.m
+++ Matlab/my_diff_smooth.m
@@ -1,4 +1,4 @@
 function D = my_diff_smooth( image, g )
-    S = reshape( image .^ cat( 3, g(1), g(2), g(3) ), [], 3);
+    S = reshape( image, [], 3) .^ (g');
     D = ln( sum( exp(mean(S) - mean(S, 'all') ).^2 ) - 2 );
 end
