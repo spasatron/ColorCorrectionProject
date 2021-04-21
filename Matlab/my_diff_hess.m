0b498f157a287a4fb67022cc9f7a54dd52f93f1a
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2021-04-21 12:41:39 -0600

    fixed numerical error

--- Matlab/my_diff_hess.m
+++ Matlab/my_diff_hess.m
@@ -1,46 +1,55 @@
 function hess = my_diff_hess( image, g )
     % Scale image, get averages
-    S = reshape( image .^ cat( 3, g(1), g(2), g(3) ), [], 3);
-    N = size( S, 1 );
-    cv = mean(S) - mean(S, 'all');
-
-    % Create cell array of nonzero values
-    S = { nonzeros(S(:,1)), nonzeros(S(:,2)), nonzeros(S(:,3)) };
+    S = reshape( image, [], 3) .^ (g');
+    cv = mean( S ) - mean( S, 'all' );
+    
+    % Get nonzero pixel values for gradient
+    pixels = reshape( image, [], 3 );
+    N = size( pixels, 1 );
+    
+    % Get nonzero values for each channel
+    Rnz = nonzeros(pixels(:,1));
+    Gnz = nonzeros(pixels(:,2));
+    Bnz = nonzeros(pixels(:,3));
     
     % Get denominator of smooth max
-    hess1 = -ones(3, 3) / ( sum( exp( cv.^2 ) ) - 2 ).^2;
-    hess2 = 2*ones(3, 3) / ( sum( exp( cv.^2 ) ) - 2 );
+    hess1 = -4*ones(3, 3) / ( sum( exp( cv.^2 ) ) - 2 ).^2;
+    hess2 =  2*ones(3, 3) / ( sum( exp( cv.^2 ) ) - 2 );
     
-    J = zeros(3, 3);
-    J2 = zeros(3, 3);
+  
+    % Compute Jacobian of first derivatives
+    J = [sum( Rnz.^g(1) .* log( Rnz ) ) ; 
+         sum( Gnz.^g(2) .* log( Gnz ) ) ; 
+         sum( Bnz.^g(3) .* log( Bnz ) ) ] .* ...
+        [ 2, -1, -1;
+         -1,  2, -1;
+         -1, -1,  2 ] / 3 / N; 
+         
+    % Compute matrix of unmixed second derivatives
+    J2 = [sum( Rnz.^g(1) .* log( Rnz ).^2 ) ; 
+          sum( Gnz.^g(1) .* log( Gnz ).^2 ) ; 
+          sum( Bnz.^g(1) .* log( Bnz ).^2 ) ] .* ...
+        [ 2, -1, -1;
+         -1,  2, -1;
+         -1, -1,  2 ] / 3 / N; 
     
-    % Compute Jacobian to get partial derivatives
-    J(1, :) = cellfun( @(Si) sum( Si.^g(1) .* log( Si ) ), S ).*[2, -1, -1]/3/N;
-    J(2, :) = cellfun( @(Si) sum( Si.^g(2) .* log( Si ) ), S ).*[-1, 2, -1]/3/N;
-    J(3, :) = cellfun( @(Si) sum( Si.^g(3) .* log( Si ) ), S ).*[-1, -1, 2]/3/N;
-    
-    % Compute derivative of each row of Jacobian with respect to g1, g2, g3
-    J(1, :) = cellfun( @(Si) sum( Si.^g(1) .* log( Si ).^2 ), S ).*[2, -1, -1]/3/N;
-    J(2, :) = cellfun( @(Si) sum( Si.^g(2) .* log( Si ).^2 ), S ).*[-1, 2, -1]/3/N;
-    J(3, :) = cellfun( @(Si) sum( Si.^g(3) .* log( Si ).^2 ), S ).*[-1, -1, 2]/3/N;
-    
-    hess1(1,1) = hess1(1,1)*sum( 2*exp( cv.^2 ) .* cv .* J(1, :) ).^2;
-    hess2(1,1) = hess2(1,1)*sum(   exp( cv.^2 ) .* ( J(1,:).^2 + cv.*J2(1,:) + 2*cv.^2.*J(1,:).^2 ) );
+    hess1(1,1) = hess1(1,1)*sum( exp( cv.^2 ) .* cv .* J(1, :) ).^2;
+    hess2(1,1) = hess2(1,1)*sum( exp( cv.^2 ) .* ( J(1,:).^2 + cv.*J2(1,:) + 2*cv.^2.*J(1,:).^2 ) );
 
-    hess1(2,2) = hess1(2,2)*sum( 2*exp( cv.^2 ) .* cv .* J(2, :) ).^2;
-    hess2(2,2) = hess2(2,2)*sum(   exp( cv.^2 ) .* ( J(2,:).^2 + cv.*J2(2,:) + 2*cv.^2.*J(2,:).^2 ) );
+    hess1(2,2) = hess1(2,2)*sum( exp( cv.^2 ) .* cv .* J(2, :) ).^2;
+    hess2(2,2) = hess2(2,2)*sum( exp( cv.^2 ) .* ( J(2,:).^2 + cv.*J2(2,:) + 2*cv.^2.*J(2,:).^2 ) );
 
-    hess1(3,3) = hess1(3,3)*sum( 2*exp( cv.^2 ) .* cv .* J(3, :) ).^2;
-    hess2(3,3) = hess2(3,3)*sum(   exp( cv.^2 ) .* ( J(3,:).^2 + cv.*J2(3,:) + 2*cv.^2.*J(3,:).^2 ) );
+    hess1(3,3) = hess1(3,3)*sum( exp( cv.^2 ) .* cv .* J(3, :) ).^2;
+    hess2(3,3) = hess2(3,3)*sum( exp( cv.^2 ) .* ( J(3,:).^2 + cv.*J2(3,:) + 2*cv.^2.*J(3,:).^2 ) );
 
-    hess1(1,2) = hess1(3,3)*sum( 2*exp( cv.^2 ).*cv.*J(1, :) )*sum( 2*exp( cv.^2 ).*cv.*J(2, :) );
-    hess2(1,2) = hess2(3,3)*sum(   exp( cv.^2 ).*J(1,:).*J(2,:).*(1 + 2*cv.^2) );
+    hess1(1,2) = hess1(1,2)*sum( exp( cv.^2 ).*cv.*J(1, :) )*sum( exp( cv.^2 ).*cv.*J(2, :) );
+    hess2(1,2) = hess2(1,2)*sum( exp( cv.^2 ).*J(1,:).*J(2,:).*(1 + 2*cv.^2) );
 
-    hess1(1,3) = hess1(3,3)*sum( 2*exp( cv.^2 ).*cv.*J(1, :) )*sum( 2*exp( cv.^2 ).*cv.*J(3, :) );
-    hess2(1,3) = hess2(3,3)*sum(   exp( cv.^2 ).*J(1,:).*J(3,:).*(1 + 2*cv.^2) );
+    hess1(1,3) = hess1(1,3)*sum( exp( cv.^2 ).*cv.*J(1, :) )*sum( exp( cv.^2 ).*cv.*J(3, :) );
+    hess2(1,3) = hess2(1,3)*sum( exp( cv.^2 ).*J(1,:).*J(3,:).*(1 + 2*cv.^2) );
 
-    hess1(2,3) = hess1(3,3)*sum( 2*exp( cv.^2 ).*cv.*J(2, :) )*sum( 2*exp( cv.^2 ).*cv.*J(3, :) );
-    hess2(2,3) = hess2(3,3)*sum(   exp( cv.^2 ).*J(2,:).*J(3,:).*(1 + 2*cv.^2) );
+    hess1(2,3) = hess1(2,3)*sum( exp( cv.^2 ).*cv.*J(2, :) )*sum( exp( cv.^2 ).*cv.*J(3, :) );
+    hess2(2,3) = hess2(2,3)*sum( exp( cv.^2 ).*J(2,:).*J(3,:).*(1 + 2*cv.^2) );
 
     % Recover symmetry
     hess1(2,1) = hess1(1, 2);   hess2(2,1) = hess2(1, 2);
