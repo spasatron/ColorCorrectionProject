29e97a23bb4443427092eceb5414430c13871972
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2021-04-21 11:57:33 -0600

    Update my_diff.m

--- Matlab/my_diff.m
+++ Matlab/my_diff.m
@@ -1,4 +1,4 @@
 function D = my_diff( image, g )
-    S = reshape( image .^ cat( 3, g(1), g(2), g(3) ), [], 3);
+    S = reshape( image, [], 3) .^ (g');
     D = max( (mean(S) - mean(S, 'all') ).^2 );
 end
