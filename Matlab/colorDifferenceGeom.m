b5541f9aa513eb53c1aabedb1aeca517d70f8206
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2021-04-23 17:45:16 -0600

    Delete colorDifferenceGeom.m

--- Matlab/colorDifferenceGeom.m
+++
@@ -1,7 +0,0 @@
-%%Function takes in an image alread in the double format and computes the
-%%max of the geomean differences vs the average.
-function D = colorDifferenceGeom(image)
-    avgs = reshape(geomean(image, [1 2]), [3, 1]);
-    totalAv = geomean(avgs);
-    D = max(abs(totalAv - avgs));
-end
